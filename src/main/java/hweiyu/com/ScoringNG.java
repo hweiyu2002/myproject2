b5278c30157ff9d18757e9b92118682d0d2e8941
hweiyu <hweiyu1001@gmail.com>
2022-03-04 15:35:19 +0800

    initial commit

---
+++ src/main/java/hweiyu/com/ScoringNG.java
@@ -0,0 +1,12 @@
+package hweiyu.com;
+
+public class ScoringNG {
+    public static void main(String[] args) {
+        String[] names = {"Jack","Hank","Karen","Elva","Benny"};
+        int[] english = {70,80,90,60,40};
+        int[] math = {88,97,54,32,75};
+        for (int i = 0; i < 5; i++) {
+            System.out.println(names[i] + "\t" + english[i] + "\t" + math[i] );
+        }
+    }
+}
