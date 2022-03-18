cb3508ae2333103e40333f2692b36941dad6a7c9
hweiyu2002 <hweiyu1001@gmail.com>
2022-03-18 15:49:39 +0800

    initial commit

---
+++ src/main/java/hweiyu/com/SalesTester.java
@@ -0,0 +1,8 @@
+package hweiyu.com;
+
+public class SalesTester {
+    public static void main(String[] args) {
+        Customer c1 = new Customer("0001",1200);
+        c1.print();
+    }
+}
---
+++ src/main/java/hweiyu/com/SilverCustomer.java
@@ -0,0 +1,8 @@
+package hweiyu.com;
+
+public class SilverCustomer extends Customer{
+    public SilverCustomer(String id, int amount) {
+        super(id, amount);
+
+    }
+}
