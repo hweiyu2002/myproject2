58b3bfa38234d4b3401e396f670707e57174b0ed
hweiyu2002 <hweiyu1001@gmail.com>
2022-03-25 16:23:56 +0800

    initial commit

---
+++ src/main/java/hweiyu/com/ticket/Station.java
@@ -0,0 +1,6 @@
+package hweiyu.com.ticket;
+
+public class Station {
+    public static final Station TAIPEI_STATION =
+            new Station(100,"Taipei");
+}
---
+++ src/main/java/hweiyu/com/ticket/Tester.java
@@ -0,0 +1,20 @@
+package hweiyu.com.ticket;
+
+import java.util.Scanner;
+
+public class Tester {
+    public static void main(String[] args) {
+        //Constructor, constant value
+        Ticket ticket = new Ticket(
+                Ticket.TAIPEI_STATION ,
+                Ticket.KAOHSIUNG_STATION);
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Your start station?(1:Taipei, 2:Taichung, 3:Kaohsiung)");
+        int startStation = Integer.parseInt(scanner.next());
+        switch (choice){
+            case 1:
+                startStation = Station.
+
+        }
+    }
+}
